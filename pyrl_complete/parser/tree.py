f52599adb285a3ed9f6eab60c1a8e94dd422279e
codimoc <codimoc@prismoid.uk>
2025-05-04 11:41:03 +0100

    tree representation

--- pyrl_complete/parser/tree.py
+++ pyrl_complete/parser/tree.py
@@ -1,12 +1,15 @@
 from typing import List, Self
 from .rules import Paths
+import re
 
 
 class Node:
+    "A node in the parse tree"
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, parent: Self):
         self.children = {}  # name -> node map for children
         self.name = name
+        self.parent = parent
 
     @classmethod
     def populate_path(cls, parent: Self, path: List):
@@ -17,13 +20,36 @@ class Node:
             return  # end of recursion
         first = path[0]
         if first not in parent.children:
-            parent.children[first] = Node(first)
-        parent = parent.children[first]
+            parent.children[first] = Node(first, parent)
         # now recurse to traverse the tree
-        cls.populate_path(parent, path[1:])
+        cls.populate_path(parent.children[first], path[1:])
 
     @classmethod
     def populate_tree(cls, parent: Self, paths: Paths):
         "Populate the entire tree from the paths"
         for p in paths:
             cls.populate_path(parent, p)
+
+    def suggestions(self) -> List[str]:
+        "Returns the suggestion at for this node"
+        return [n.name for n in self.children]
+
+    def regexp(self) -> str:
+        if self.name == "root":
+            return "^"
+        name = self.name
+        # TODO: substitute ? with \w+
+        return f"{self.parent.regexp()}\s+{name}"
+
+
+class Tree:
+    "The full parse tree represntation of the grammar"
+
+    def __init__(self, root: Node):
+        self.root = root
+        self.cache = {}  # a map from partial string input to Node
+
+    def find_node(self, input: str) -> Node:
+        node = self.root
+        if input in self.cache:
+            return self.cache[input]
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -3,9 +3,10 @@ from pyrl_complete.parser import Node
 
 def test_simple_tree():
     paths = [["get", "one"], ["get", "two"], ["set"]]
-    root = Node("root")
+    root = Node("root", None)
     Node.populate_tree(root, paths)
     assert len(root.children) == 2
     assert len(root.children["get"].children) == 2
+    assert root.children["get"].parent.name == "root"
+    assert root.children["get"].children["one"].parent.name == "get"
     assert len(root.children["set"].children) == 0
-    
